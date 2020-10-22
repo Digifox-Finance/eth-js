edd25d39c1721cd9be3a540a4ed10e0a15624e8c
chasepoirier <chase@chasepoirier.com>
2020-10-22 17:50:32 -0400

    re-export ethjs-util

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import createWallet from "./createWallet";
 import signTransaction from "./signTransaction";
 
 export * from "ethereumjs-tx";
+export * from "ethjs-util";
 
 export { createWallet, signTransaction };
